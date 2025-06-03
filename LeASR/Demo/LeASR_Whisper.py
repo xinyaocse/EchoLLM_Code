821f21d33720cfe73fae29ac27df42fed1c23eeb
hk-c <70185913+hk-c@users.noreply.github.com>
2025-06-03 19:20:56 +0800

    Add files via upload

---
+++ LeASR/Demo/LeASR_Whisper.py
@@ -0,0 +1,17 @@
+from transformers import SpeechT5Processor, SpeechT5ForSpeechToText, WhisperProcessor, WhisperForConditionalGeneration
+import torch
+import librosa
+
+processor = WhisperProcessor.from_pretrained("/Whisper")
+model = WhisperForConditionalGeneration.from_pretrained("/Whisper")
+
+audio_file = "/distance/60/audio01_Raw_0.wav"
+audio, sr = librosa.load(audio_file, sr=processor.feature_extractor.sampling_rate)
+
+inputs = processor(audio=audio, sampling_rate=sr, return_tensors="pt")
+
+predicted_ids = model.generate(**inputs, max_length=100)
+
+transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+print("Transcription:", transcription[0])
